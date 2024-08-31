10a5ae3c9b3ce69aab43f68359b46c0d466b28f1
pedroping <pedrpchavesdias@gmail.com>
2024-08-31 11:28:31 -0300

    Feat: create header and footer

---
+++ src/app/core/ui/header/ui/header.component.spec.ts
@@ -0,0 +1,3 @@
+/* tslint:disable:no-unused-variable */
+
+describe('HeaderComponent', () => {});
---
+++ src/app/domain/home/page/home-page.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { FooterComponent } from '@core/ui/footer';
+import { HeaderComponent } from '@core/ui/header';
+
+@Component({
+  selector: 'info-home-page',
+  templateUrl: './home-page.component.html',
+  styleUrls: ['./home-page.component.scss'],
+  standalone: true,
+  imports: [FooterComponent, HeaderComponent, RouterOutlet],
+})
+export class HomePageComponent {}
