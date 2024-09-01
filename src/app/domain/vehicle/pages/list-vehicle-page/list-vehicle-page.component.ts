a36f0a1d080c007514e3fac1f989625e3479aeb5
pedroping <pedrpchavesdias@gmail.com>
2024-09-01 18:55:45 -0300

    Feat: add no data content

--- src/app/domain/vehicle/pages/list-vehicle-page/list-vehicle-page.component.ts
+++ src/app/domain/vehicle/pages/list-vehicle-page/list-vehicle-page.component.ts
@@ -2,15 +2,18 @@ import { AsyncPipe } from '@angular/common';
 import { Component, inject } from '@angular/core';
 import { VehiclesFacade } from '@core/services/facades';
 import { VehicleComponent } from '../../components/vehicle/vehicle.component';
+import { eRoutes } from '@shared/enums';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'info-list-vehicle-page',
   templateUrl: './list-vehicle-page.component.html',
   styleUrls: ['./list-vehicle-page.component.scss'],
   standalone: true,
-  imports: [VehicleComponent, AsyncPipe],
+  imports: [VehicleComponent, AsyncPipe, RouterLink],
 })
 export class ListVehiclePageComponent {
+  newVehicleRoute = `${eRoutes.VEHICLE}/${eRoutes.VEHICLE_NEW}`;
   private readonly vehiclesFacade = inject(VehiclesFacade);
   vehicles$ = this.vehiclesFacade.getVehicles$$();
 }
