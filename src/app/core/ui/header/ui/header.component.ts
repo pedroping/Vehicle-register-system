532e687ba0cbd7887b4979a28ebe4a19e131f7ac
pedroping <pedro.dias@moneyprestador.com.br>
2025-10-29 13:41:45 -0300

    Feat: create new header

--- src/app/core/ui/header/ui/header.component.ts
+++ src/app/core/ui/header/ui/header.component.ts
@@ -1,22 +1,20 @@
-import { isPlatformServer } from '@angular/common';
 import {
+  afterNextRender,
   Component,
   DestroyRef,
   ElementRef,
   inject,
-  Inject,
-  OnInit,
-  PLATFORM_ID,
+  viewChild,
 } from '@angular/core';
 import { takeUntilDestroyed } from '@angular/core/rxjs-interop';
-import { Router, RouterLink } from '@angular/router';
+import { NavigationEnd, Router, RouterLink } from '@angular/router';
 import {
   FaIconLibrary,
   FontAwesomeModule,
 } from '@fortawesome/angular-fontawesome';
 import { faCar } from '@fortawesome/free-solid-svg-icons';
 import { eRoutes } from '@shared/enums';
-import { debounceTime, fromEvent, tap } from 'rxjs';
+import { filter, fromEvent, startWith, switchMap } from 'rxjs';
 @Component({
   selector: 'info-header',
   templateUrl: './header.component.html',
@@ -27,7 +25,36 @@ export class HeaderComponent {
   vehicleListRoute = eRoutes.VEHICLE;
   newVehicleRoute = `${eRoutes.VEHICLE_NEW}`;
 
+  private readonly header =
+    viewChild.required<ElementRef<HTMLElement>>('header');
+  private readonly router = inject(Router);
+  private readonly destroyRef = inject(DestroyRef);
+
   constructor(library: FaIconLibrary) {
     library.addIcons(faCar);
+
+    afterNextRender(() => {
+      this.scrollListener();
+    });
+  }
+
+  private scrollListener() {
+    this.router.events
+      .pipe(
+        filter((event) => event instanceof NavigationEnd),
+        startWith(null),
+        switchMap(() => fromEvent(window, 'scroll').pipe(startWith(null))),
+        takeUntilDestroyed(this.destroyRef)
+      )
+      .subscribe(() => {
+        const isAtHome = this.router.url === '/';
+        const higherThanHeader =
+          window.document.documentElement.scrollTop > 150;
+
+        this.header().nativeElement.style.transform =
+          higherThanHeader || !isAtHome
+            ? 'translateY(0px)'
+            : 'translateY(-60px)';
+      });
   }
 }
--- src/app/domain/vehicle/components/list-vehicle-header/list-vehicle-header.component.ts
+++ src/app/domain/vehicle/components/list-vehicle-header/list-vehicle-header.component.ts
@@ -1,11 +1,22 @@
-import { AsyncPipe } from '@angular/common';
+import { Component } from '@angular/core';
+import { RouterLink } from '@angular/router';
 import {
-  Component
-} from '@angular/core';
+  FaIconLibrary,
+  FontAwesomeModule,
+} from '@fortawesome/angular-fontawesome';
+import { faCar } from '@fortawesome/free-solid-svg-icons';
+import { eRoutes } from '@shared/enums';
 
 @Component({
   selector: 'info-list-vehicle-header',
   templateUrl: './list-vehicle-header.component.html',
   styleUrls: ['./list-vehicle-header.component.scss'],
+  imports: [RouterLink, FontAwesomeModule],
 })
-export class ListVehicleHeaderComponent {}
+export class ListVehicleHeaderComponent {
+  newVehicleRoute = `${eRoutes.VEHICLE_NEW}`;
+
+  constructor(library: FaIconLibrary) {
+    library.addIcons(faCar);
+  }
+}
