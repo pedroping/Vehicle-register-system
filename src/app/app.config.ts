e2f53586f146a93f396a117c2194862c4be663b5
pedroping <pedro.dias@moneyprestador.com.br>
2025-11-13 22:31:21 -0300

    Feat: install pwa

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,14 +1,15 @@
 import { IMAGE_CONFIG } from '@angular/common';
 import { provideHttpClient, withFetch, withInterceptors } from '@angular/common/http';
-import { ApplicationConfig, Provider, provideZoneChangeDetection } from '@angular/core';
+import { ApplicationConfig, isDevMode, Provider, provideZoneChangeDetection } from '@angular/core';
 import { provideClientHydration } from '@angular/platform-browser';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 import {
   provideRouter,
   RouteReuseStrategy,
   withInMemoryScrolling,
-  withViewTransitions
+  withViewTransitions,
 } from '@angular/router';
+import { provideServiceWorker } from '@angular/service-worker';
 import { environment } from '@environment/environment';
 import { errorHandleInterceptor, loadingHttpInterceptorFn } from '@interceptors';
 import { loadingSpinnerProvider } from '@providers';
@@ -52,5 +53,9 @@ export const appConfig: ApplicationConfig = {
     ENVIRONMENT_PROVIDER,
     provideClientHydration(),
     { provide: RouteReuseStrategy, useClass: CustomRouteReuseStrategy },
+    provideServiceWorker('ngsw-worker.js', {
+      enabled: !isDevMode(),
+      registrationStrategy: 'registerWhenStable:30000',
+    }),
   ],
 };
